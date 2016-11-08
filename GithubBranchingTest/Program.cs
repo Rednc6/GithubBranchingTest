6a92c968f08291a54a2f50ab42572ae01255c4e4
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-11-08 10:45:01 +0100

    Add project files.

---
+++ GithubBranchingTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubBranchingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testing branching via Github");
+        }
+    }
+}
